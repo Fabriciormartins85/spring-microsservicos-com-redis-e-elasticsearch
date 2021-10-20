21b87b69743f9227f362e19aaa0942e6d1096796
Fabrício Martins <fabriciormartins85@gmail.com>
2021-10-20 01:54:28 -0300

    Add centralizador de configuracoes

---
+++ estudo-microsservicos-server-config/src/test/java/br/com/example/serverconfig/EstudoMicrosservicosServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.serverconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoMicrosservicosServerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
